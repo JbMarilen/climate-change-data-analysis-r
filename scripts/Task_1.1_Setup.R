77efb3fe8997a4bdc93eeb60313f403d3f2df30d
JbMarilen <nemojbmarilen@gmail.com>
2026-03-16 22:02:23 +0000

    rename outputs/ to charts/

--- scripts/Task_1.1_Setup.R
+++ scripts/Task_1.1_Setup.R
@@ -1,12 +1,12 @@
-# Task 1.1: Install and load ggplot2, and load the climate change dataset
+# Task 1.1: Install and load ggplot2 (through tidyverse, allowing for dplyr and ggplot2), and load the climate change dataset
 
-# Install ggplot2 if it's not already installed
-if (!require(ggplot2)) {
-  install.packages("ggplot2")
+# Install tidyverse if it's not already installed
+if (!require(tidyverse)) {
+  install.packages("tidyverse")
 }
 
-# Load the ggplot2 library
-library(ggplot2)
+# Load the tidyverse library
+library(tidyverse)
 
 # Load the climate change dataset
 climate <- read.csv("data/Beginner_Climate_Change_Dataset_20_Features_1200_Rows.csv")
