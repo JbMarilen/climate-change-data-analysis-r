543e2418853ffd3f3b50a85b9de572e03162fc5e
JbMarilen <nemojbmarilen@gmail.com>
2026-03-25 22:43:08 +0000

    Update Task 6 and remove .DS_Store

--- scripts/Task_6_Bar_Chart.r
+++ scripts/Task_6_Bar_Chart.r
@@ -4,7 +4,15 @@ source("scripts/Task_1.1_Setup.R")
 climateRI_summary <- climate %>%
     group_by(country) %>%
     summarize(average_climateRI = mean(climate_risk_index)) %>%
-    arrange(average_climateRI) %>%
+    arrange(average_climateRI)
 
-Bar_Plot <- ggplot(climateRI_summary, aes(x = reorder(country, average_climateRI), y = average_climateRI)) + geom_col() + labs(title = "Average Climate Risk Index per Country", x = "Country", y = "Average Climate Risk Index")
-
+Bar_Plot <- ggplot(climateRI_summary, aes(
+    x = reorder(country, average_climateRI), 
+    y = average_climateRI
+)) + 
+    geom_col() + 
+    labs(
+        title = "Average Climate Risk Index per Country", 
+        x = "Country", 
+        y = "Average Climate Risk Index"
+        )
