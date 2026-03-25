898f081ec5b4e87db0a8bb725076f73001381a67
Atharv Soni <atharvsoni@Atharvs-MacBook-Air.local>
2026-03-25 23:03:13 +0000

    Updated task 6 with comments and save statement

--- scripts/Task_6_Bar_Chart.r
+++ scripts/Task_6_Bar_Chart.r
@@ -1,18 +1,29 @@
 # Task 6: Bar Chart
 source("scripts/Task_1.1_Setup.R")
 
+# Group the data by country to compute the mean of the climate_risk_index
 climateRI_summary <- climate %>%
+    # the rows get grouped by unique country names
     group_by(country) %>%
+    # this calculates the average of the climate_risk_index for each country
     summarize(average_climateRI = mean(climate_risk_index)) %>%
+    # sorts the data table by the mean value in assending order
     arrange(average_climateRI)
 
-Bar_Plot <- ggplot(climateRI_summary, aes(
+# creates the bar chart
+Bar_Chart <- ggplot(climateRI_summary, aes(
+    # "reorder" ensures that the countries that appear on the x-axis are in assending order of thier corresponding mean values of climate_risk_index
     x = reorder(country, average_climateRI), 
     y = average_climateRI
 )) + 
+    # draws the type of chart in our case bar chart
     geom_col() + 
+    # add a title and names the x and y axis
     labs(
         title = "Average Climate Risk Index per Country", 
         x = "Country", 
         y = "Average Climate Risk Index"
         )
+
+print(Bar_Chart)
+ggsave("charts/task6")
