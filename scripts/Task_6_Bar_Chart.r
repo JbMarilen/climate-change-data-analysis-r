c9604d465bbd86ba753e944d042050ef6e1f4fb3
Atharv Soni <atharvsoni@Atharvs-MacBook-Air.local>
2026-03-25 22:20:08 +0000

    testing task 6

--- scripts/Task_6_Bar_Chart.r
+++ scripts/Task_6_Bar_Chart.r
@@ -1,1 +1,10 @@
-print("hello world")
+# Task 6: Bar Chart
+source("scripts/Task_1.1_Setup.R")
+
+climateRI_summary <- climate %>%
+    group_by(country) %>%
+    summarize(average_climateRI = mean(climate_risk_index)) %>%
+    arrange(average_climateRI) %>%
+
+Bar_Plot <- ggplot(climateRI_summary, aes(x = reorder(country, average_climateRI), y = average_climateRI)) + geom_col() + labs(title = "Average Climate Risk Index per Country", x = "Country", y = "Average Climate Risk Index")
+
