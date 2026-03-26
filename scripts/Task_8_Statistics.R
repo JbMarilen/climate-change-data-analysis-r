725ab61959a59fc7e54b90d947ebd7274abdd2f9
Kacper Szymoniak <kacper.szymoniak2@mail.dcu.ie>
2026-03-26 16:36:58 +0000

    trying task 8

--- scripts/Task_8_Statistics.R
+++ scripts/Task_8_Statistics.R
@@ -1,3 +1,15 @@
 
 source("scripts/Task_1.1_Setup.R") #load df and dplyr
+source("scripts/Task_4_Dataset_Manipulation")
 
+statistic_func <- function(df) {
+    stats_full <- list(
+        m = mean(df),
+        v = var(df),
+        std = sd(df)
+    )
+}
+
+#finding the statistics of the expected average temperature 2050 dataframe
+stats_2050 <- statistic_func(country_expected_avg_temperatures_2050.csv)
+print(stats_2050)
