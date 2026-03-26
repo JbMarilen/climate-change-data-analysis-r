961eaf179f2c1be1beca78901e0189ec939befe8
Atharv Soni <atharvsoni@Atharvs-MacBook-Air.local>
2026-03-26 22:57:54 +0000

    update to task 3

---
+++ scripts/Task_3_Function_And_Data_Manipulation.r
@@ -0,0 +1,16 @@
+# Task 3: Function and Data Manipulation
+
+source("scripts/Task_1.1_Setup.R") # load df and dplyr
+
+Temp_Data <- climate %>%
+    select(max_temperature, min_temperature, sea_surface_temperature, global_avg_temperature)
+
+Temp_Diff <- function(df, col_a, col_b) {
+    df[[col_b]] - df[[col_b]]
+}
+
+climate_temperature_differences <- Temp_Data %>%
+    mutate(
+        temperature_diff = Temp_Diff(Temp_Data, "max_temperature", "min_temperature"),
+        seasurface_air_temp_diff = Temp_Diff(Temp_Data, "sea_surface_temperature", "global_avg_temperature")
+        )
