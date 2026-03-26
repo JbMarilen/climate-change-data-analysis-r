e6fb05a84e0767bedc235b444e4540b2648daaf6
Atharv Soni <Atharv_soni@icloud.com>
2026-03-26 16:54:23 +0000

    update2

--- scripts/Task_10_Function_And_Filter.r
+++ scripts/Task_10_Function_And_Filter.r
@@ -2,10 +2,14 @@
 
 source("scripts/Task_1.1_Setup.R")
 
+# question : Create a function that evaluates all countries based on their average climate_risk_index, and then returns the 2 countries with the lowest climate_risk_index.
 Lowest_ClimateRI <- function(x) {
     climate %>%
+        # the rows in the climate df get grouped by unique country names
         group_by(country) %>%
+        # this calculates the average climate risk index for each county
         summarize(average_climateRI = mean(climate_risk_index)) %>%
+        # this returns the the lowest 2 country with the smallest climate risk index
         slice_min(average_climateRI, n = 2)
 }
 
