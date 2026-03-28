79f124cba1a67f0ec82d23a2ab5bdece5e392322
Atharv-soni-07 <atharv_soni@icloud.com>
2026-03-28 14:27:05 +0000

    added rds and comments

--- scripts/Task_3_Function_And_Data_Manipulation.r
+++ scripts/Task_3_Function_And_Data_Manipulation.r
@@ -6,7 +6,7 @@ Temp_Data <- climate %>%
     select(max_temperature, min_temperature, sea_surface_temperature, global_avg_temperature)
 
 Temp_Diff <- function(df, col_a, col_b) {
-    df[[col_b]] - df[[col_b]]
+    df[[col_a]] - df[[col_b]]
 }
 
 climate_temperature_differences <- Temp_Data %>%
@@ -14,3 +14,5 @@ climate_temperature_differences <- Temp_Data %>%
         temperature_diff = Temp_Diff(Temp_Data, "max_temperature", "min_temperature"),
         seasurface_air_temp_diff = Temp_Diff(Temp_Data, "sea_surface_temperature", "global_avg_temperature")
         )
+
+saveRDS(climate_temperature_differences, file = "data/climate_temperature_differences.rds")
