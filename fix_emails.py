400dd53f6f25ff408e417a88c16e829092468b70
JbMarilen <nemojbmarilen@gmail.com>
2026-03-29 00:00:44 +0000

    email fix perchance 2

--- fix_emails.py
+++ fix_emails.py
@@ -1,7 +1,15 @@
+import subprocess
+
+script = """
 email_map = {
     b"kacpers@Kacpers-MacBook-Air.local": b"kacper.szymoniak2@mail.dcu.ie",
 }
+commit.author_email = email_map.get(commit.author_email, commit.author_email)
+commit.committer_email = email_map.get(commit.committer_email, commit.committer_email)
+"""
 
-def callback(commit, metadata):
-    commit.author_email = email_map.get(commit.author_email, commit.author_email)
-    commit.committer_email = email_map.get(commit.committer_email, commit.committer_email)
+subprocess.run([
+    "python", "-m", "git_filter_repo",
+    "--force",
+    "--commit-callback", script
+])
