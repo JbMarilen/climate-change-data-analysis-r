e89838e0cac15454a6b335a4a7f1e69fce8048c9
kacper.szymoniak2 <kacper.szymoniak2@mail.dcu.ie>
2026-04-05 22:28:06 +0100

    report for task 5, 7 and 8

--- scripts/Task_9_Subsets.R
+++ scripts/Task_9_Subsets.R
@@ -4,10 +4,13 @@ source("scripts/Task_1.1_Setup.R") # load df, dplyr, ggplot2
 climate_temperature_differences <- readRDS("data/climate_temperature_differences.rds")
 
 only_uk <- climate %>%
+  # The rows of the country df get filtered by the country UK
   filter(country == "UK")
 
+# The first 100 rows of the new df are kept and retained. 
 first_100_uk <- only_uk[1:100,]
 
+# This forms a scatter plot with the 100 rows, with co2 concentration over time
 scatter_uk_100 <- ggplot(first_100_uk, aes(x = year, y = co2_concentration_ppm)) +
   geom_point() +
   labs(
@@ -16,5 +19,6 @@ scatter_uk_100 <- ggplot(first_100_uk, aes(x = year, y = co2_concentration_ppm))
     y = "Co2 Concentration (ppm)"
   )
 
+#Prints and saves the scatter plot as a png
 print(scatter_uk_100)
 ggsave("charts/Task_9_Co2_UK_100.png", plot = scatter_uk_100)
